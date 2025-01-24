ca3bd6e982828535ecb713c75fdeada0473102b5
Prince Babu <princebabu4495@gmail.com>
2025-01-24 14:03:52 +0530

    new add

--- vite.config.js
+++ vite.config.js
@@ -12,25 +12,25 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: 'My PWA App',
-        short_name: 'Instragram0',
+        name: 'My Clock',
+        short_name: 'Clock',
         description: 'A React app with PWA capabilities',
         theme_color: '#ffffff',
         "display": "standalone",
         
         icons: [
           {
-            src: 'https://i.pinimg.com/736x/19/42/d5/1942d5deb0f788e6228054cd92767ff6.jpg',
+            src: 'https://i.pinimg.com/736x/1a/86/91/1a8691c006f7bcea76fea15009c706a6.jpg',
             sizes: '192x192',
             type: 'image/jpg',
           },
           {
-            src: 'https://i.pinimg.com/736x/19/42/d5/1942d5deb0f788e6228054cd92767ff6.jpg',
+            src: 'https://i.pinimg.com/736x/1a/86/91/1a8691c006f7bcea76fea15009c706a6.jpg',
             sizes: '512x512',
             type: 'image/jpg',
           },
           {
-            src: 'https://i.pinimg.com/736x/19/42/d5/1942d5deb0f788e6228054cd92767ff6.jpg',
+            src: 'https://i.pinimg.com/736x/1a/86/91/1a8691c006f7bcea76fea15009c706a6.jpg',
             sizes: '512x512',
             type: 'image/jpg',
             purpose: 'any maskable',
